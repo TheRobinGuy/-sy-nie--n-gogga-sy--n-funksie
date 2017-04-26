13056fca6062d2e1f520b3f4a42c1832d77aa06a
mucklagh28 <kogrady567@gmail.com>
2017-04-26 17:25:32 +0100

    Add files via upload

---
+++ HexaFlip/js/myplaylist.js
@@ -0,0 +1,53 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+{
+        mp3:'mix/Kalimba.mp3',
+        //oga:'mix/1.ogg',
+        title:'Kalimba',
+        artist:'Mr. Scruff',
+        rating:4,
+        buy:'https://www.amazon.com/MP3-Music-Download/b?ie=UTF8&node=163856011',
+        price:'1.99',
+        duration:'5:48',
+        cover:'mix/NinjaTuna.jpg'
+    },
+    {
+        mp3:'mix/Maid with the Flaxen Hair.mp3',
+        //oga:'mix/1.ogg',
+        title:'Maid with the Flaxen Hair',
+        artist:'Richard Stoltzman',
+        rating:1,
+        buy:'https://www.amazon.com/MP3-Music-Download/b?ie=UTF8&node=163856011',
+        price:'0.49',
+        duration:'2:49',
+        cover:'mix/maid.jpg'
+    },
+    {
+        mp3:'mix/Sleep Away.mp3',
+        //oga:'mix/1.ogg',
+        title:'Sleep Away',
+        artist:'Bob Acri',
+        rating:2,
+        buy:'https://www.amazon.com/MP3-Music-Download/b?ie=UTF8&node=163856011',
+        price:'1.49',
+        duration:'3:20',
+        cover:'mix/sleep.jpeg'
+    },
+    {
+        mp3:'mix/Where Have You Been.mp3',
+        title:'Where Have You Been',
+        artist:'Reel Big Fish',
+        rating:5,
+        buy:'https://www.amazon.com/MP3-Music-Download/b?ie=UTF8&node=163856011',
+        price:'1.99',
+        duration:'3:24',
+        cover:'mix/fish.jpg'
+    }
+];
